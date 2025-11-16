7e12db68dc949a6bdcee69eab1767c758db13098
Balaji112004 <k.balaji112004@gmail.com>
2025-11-16 19:53:11 +0530

    Update CORS origin for PaymentController

--- src/main/java/com/signuplogin/demo/controllers/PaymentController.java
+++ src/main/java/com/signuplogin/demo/controllers/PaymentController.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://bookstore-frontend-eta-six.vercel.app")
 @RestController
 @RequestMapping("/api/orders")
 public class PaymentController {
